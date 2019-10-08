c5f86bfff5fb65f8d6bdd0c3179675d7e08704cb
kayak <41324271+by-k4y4k@users.noreply.github.com>
2019-10-08 15:00:48 +1000

    Bring in Google Fonts properly

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,62 +1,21 @@
-const config = require('./data/config');
-
 module.exports = {
-	siteMetadata: {
-		site_url: config.url
-	},
 	plugins: [
 		'gatsby-plugin-react-helmet',
 		'gatsby-plugin-sass',
 		'gatsby-plugin-styled-components',
-		'gatsby-plugin-netlify',
-		'gatsby-plugin-catch-links',
 		{
-			resolve: 'gatsby-plugin-google-fonts',
+			resolve: 'gatsby-plugin-web-font-loader',
 			options: {
-				fonts: ['Cairo', 'Roboto']
-			}
-		},
-		{
-			resolve: 'gatsby-plugin-canonical-urls',
-			options: {
-				siteUrl: config.url
-			}
-		},
-		{
-			resolve: 'gatsby-plugin-google-analytics',
-			options: {
-				trackingId: config.googleAnalyticsID,
-				head: true
+				google: {
+					families: ['Barlow Condensed', 'Open Sans']
+				}
 			}
 		},
 		{
 			resolve: 'gatsby-plugin-nprogress',
 			options: {
-				color: config.themeColor,
+				color: '#22d07f',
 				showSpinner: false
-			}
-		},
-		{
-			resolve: 'gatsby-plugin-manifest',
-			options: {
-				name: 'Gatsby',
-				short_name: 'Gatsby',
-				start_url: '/',
-				background_color: config.backgroundColor,
-				theme_color: config.themeColor,
-				display: 'minimal-ui',
-				icons: [
-					{
-						src: '/favicon/logo-192x192.png',
-						sizes: '192x192',
-						type: 'image/png'
-					},
-					{
-						src: '/favicon/logo-512x512.png',
-						sizes: '512x512',
-						type: 'image/png'
-					}
-				]
 			}
 		}
 		// 'gatsby-plugin-offline'
